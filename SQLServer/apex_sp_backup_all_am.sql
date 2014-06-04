febb95d3d04c97ee900db2d189d2454757a7e896
Stefano Teodorani <teopost@gmail.com>
2014-06-04 10:31:16 +0200

    Update apex_sp_backup_all_am.sql

--- SQLServer/apex_sp_backup_all_am.sql
+++ SQLServer/apex_sp_backup_all_am.sql
@@ -1,4 +1,26 @@
-create  proc [dbo].[apex_sp_backup_all_am] @backupDir varchar(50)
+/*
+
+Backup of all AM_% database
+
+Backup Sample:
+
+exec apex_sp_backup_all_am  'c:\temp'
+go
+
+*/
+
+USE [master]
+GO
+
+/****** Object:  StoredProcedure [dbo].[sp_backup_db]    Script Date: 07/03/2014 18.23.35 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+create proc [dbo].[apex_sp_backup_all_am] @backupDir varchar(50)
 as
 declare @dbname sysname
 begin 
